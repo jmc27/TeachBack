7aca2cc495e284788a461851a5ded343d9a72bef
William Tarimo <wtarimo@brandeis.edu>
2014-02-13 22:46:02 -0500

    rails new TeachBack + Extra Gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TeachBack::Application.config.secret_key_base = '2fc92542b4d1abae09dabf2ab95b9b150da85afc7ffb7dc9ce6b1224c64bfaca425cf08a87f113ca7d544493992cef4b1920a24a13e77814754176ed0a8dde79'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TeachBack::Application.config.session_store :cookie_store, key: '_TeachBack_session'
