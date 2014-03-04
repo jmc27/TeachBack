2360dadf35a3caec63bd35b9b6fb410fe363a63d
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-03-03 23:24:13 -0500

    Added first and last name fields to users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :configure_permitted_parameters, if: :devise_controller?
 
   #Route to user page on sign-in
   def after_sign_in_path_for(resource)
     user_path(current_user)
+  end
+
+  protected
+
+  #Allow additional parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304015517) do
+ActiveRecord::Schema.define(version: 20140304040748) do
 
   create_table "attendances", force: true do |t|
     t.integer  "lecture_id"
@@ -159,6 +159,8 @@ ActiveRecord::Schema.define(version: 20140304015517) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
