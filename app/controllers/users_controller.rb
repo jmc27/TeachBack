6c7d7fefa1603b7f12bb114682c52031346ea838
jmc27 <jmc27@brandeis.edu>
2014-03-09 14:38:10 -0400

    new Assets
    carousel cycling but navigation not working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,21 @@ class UsersController < ApplicationController
   def set_user
     @user = current_user
   end
+
+  #Route to user page on sign-in
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  def after_updated_account_path_for(resource)
+  	user_path(current_user)
+  end
+
+  protected
+
+  #Allow additional parameters
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:account_update) << :first_name
+    devise_parameter_sanitizer.for(:account_update) << :last_name
+  end
 end
