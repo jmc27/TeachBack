c7235a78103f15a27acf381d25694c946a43addc
berksarioz <bsarioz@gmail.com>
2014-03-09 16:18:18 -0400

    changes

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,7 +1,5 @@
 class Notification < ActiveRecord::Base
-	belongs_to :course
-	belongs_to :lecture
-	belongs_to :user
+	belongs_to :course, :lecture, :user
 
 	validates :course_id, :user_id, :message, presence: true
 end
