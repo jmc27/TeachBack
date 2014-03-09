27a9029fd81a9301e44ee4fa0164f652306dd071
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-03-09 16:29:27 -0400

    Fixed courses table

--- db/migrate/20140309182317_create_courses.rb
+++ db/migrate/20140309182317_create_courses.rb
@@ -1,6 +1,8 @@
 class CreateCourses < ActiveRecord::Migration
   def change
     create_table :courses do |t|
+	t.integer :owner_id
+	t.string :title
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140309194402) do
+ActiveRecord::Schema.define(version: 20140309182317) do
 
   create_table "attendances", force: true do |t|
     t.integer  "lecture_id"
@@ -32,6 +32,8 @@ ActiveRecord::Schema.define(version: 20140309194402) do
   end
 
   create_table "courses", force: true do |t|
+    t.integer "owner_id"
+    t.string  "title"
   end
 
   create_table "feedbacks", force: true do |t|
@@ -151,23 +153,5 @@ ActiveRecord::Schema.define(version: 20140309194402) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-
-  create_table "views", force: true do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "views", ["email"], name: "index_views_on_email", unique: true
-  add_index "views", ["reset_password_token"], name: "index_views_on_reset_password_token", unique: true
 
 end
