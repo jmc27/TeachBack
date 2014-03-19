d46afb1d537ca31a7c3ba2eea8281deda59c4b9d
Nicholas Cummins <nicholas.r.cummins@gmail.com>
2014-03-19 12:14:46 -0400

    added username index

--- db/migrate/20140313183720_add_pins_to_courses.rb
+++ db/migrate/20140313183720_add_pins_to_courses.rb
@@ -3,8 +3,6 @@ class AddPinsToCourses < ActiveRecord::Migration
   	add_column :courses, :instructor_pin, :integer
   	add_column :courses, :student_pin, :integer
   	add_column :courses, :location, :string
+ 	add_index :users, :username,                :unique => true
   end
-
-  add_index :users, :username,                :unique => true
-
 end
