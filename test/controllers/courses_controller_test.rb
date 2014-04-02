9cfe5b395ba582ca9a9420601e4cf3c1a431e319
jmc27 <jmc27@brandeis.edu>
2014-04-01 21:25:00 -0400

    course controller tests
    newPINS fails

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -1,7 +1,44 @@
 require 'test_helper'
 
-class CoursesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+describe "valid course" do
+	before do
+		@course = Course.new(title: "Intro to CS",
+							code: "COSI 101",
+							instructor_pin: 1231453,
+							student_pin: 4349493,
+							lecture_days: "M,W,F",
+							lecture_start_time: "10:00",
+							lecture_end_time: "10:50",
+							start_date: Date.new(2014,1,14),
+							end_date: Date.new(2014,5,11),
+							semester: "Spring 2014",
+							school: "Harvard University",
+							location: "Volen 101",
+							instructor: "Some teacher")
+	end
+
+	it "tests valid course creation" do
+		@course.valid?.must_equal true
+	end
+end
+
+describe "pin method" do
+	before do
+		@course = Course.new(title: "Intro to CS",
+							code: "COSI 101",
+							lecture_days: "M,W,F",
+							lecture_start_time: "10:00",
+							lecture_end_time: "10:50",
+							start_date: Date.new(2014,1,14),
+							end_date: Date.new(2014,5,11),
+							semester: "Spring 2014",
+							school: "Harvard University",
+							location: "Volen 101",
+							instructor: "Teacher something")
+		@course.instructor_pin = @course.newPINS
+		@course.student_pin = @course.newPINS
+	end
+	it "test the newPINS method" do
+		@course.valid?.must_equal true
+	end
 end
