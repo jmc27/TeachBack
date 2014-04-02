8a5b6db8987c0d127a6e2de07b56c529010b1a73
jmc27 <jmc27@brandeis.edu>
2014-04-01 20:56:25 -0400

    moved test to controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
-class UsersControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
+
+
+
+	describe "valid user" do
+		before do 
+			@user = User.new(first_name:"John",
+			last_name: "Smith",
+			username: "jsmith",
+			password: "jsmith2014",
+			email: "jsmith@email.com")
+		end
+		it "creates user successfully with valid params" do
+			@user.valid?.must_equal true
+		end
+	end
+	describe "invalid user" do
+		before do
+		  @user = User.new(first_name: "John",
+			last_name: "Smith",
+			username: "jsmith",
+			password: "jsmith2014",
+			email: "jsmith@email")
+		end
+		it "is invalid with an invalid email" do
+		  @user.valid?.must_equal false
+		end
+	end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,29 +1,3 @@
 require 'test_helper'
 
 
-
-	describe "valid user" do
-		before do 
-			@user = User.new(first_name:"John",
-			last_name: "Smith",
-			username: "jsmith",
-			password: "jsmith2014",
-			email: "jsmith@email.com")
-		end
-		it "creates user successfully with valid params" do
-			@user.valid?.must_equal true
-		end
-	end
-	describe "invalid user" do
-		before do
-		  @user = User.new(first_name: "John",
-			last_name: "Smith",
-			username: "jsmith",
-			password: "jsmith2014",
-			email: "jsmith@email")
-		end
-		it "is invalid with an invalid email" do
-		  @user.valid?.must_equal false
-		end
-	end
-
